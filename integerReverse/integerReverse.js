2e23d0b5abcf3d513329396d21abf750ba4f543b
Dwayne Draper <deandraper82@gmail.com>
2020-10-24 17:05:44 -0500

    Refactored code recursively to handle numbers of any size without need for a crazy if else tree.

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -9,34 +9,20 @@
  *
  */
 
-function reverseInteger(number) {
-  let places = 0;
-  let result = 0;
-  // TODO: Implement this function!
-  if (number < 10) {
-    places = 1;
-  } else if (number < 100) {
-    places = 2;
-  } else if (number < 1000) {
-    places = 3;
-  } else if (number < 10000) {
-    places = 4;
-  } else if (number < 100000) {
-    places = 5;
-  } else if (number < 1000000) {
-    places = 6;
-  } else {
-    console.log("I give up.");
-    return "I give up.";
+function reverseInteger(num) {
+  let res = 0;
+  let mod = 0;
+  function recurse(num, res) {
+    if (num < 10) {
+      res += num;
+      return res;
+    }
+    mod = num % 10;
+    num = (num - mod) / 10;
+    res = (res * 10) + (mod * 10);
+    return recurse(num, res)
   }
-  for (let i = places - 1; i >= 1; i--) {
-    let modulo = number % 10;
-    let numWithoutModulo = Math.floor((number - modulo) / 10);
-    number = numWithoutModulo;
-    result = result + modulo * 10 ** i;
-  }
-  result += number;
-  return result;
+  return recurse(num, res);
 }
 console.log(reverseInteger(3567));
 console.log(reverseInteger(271514));
