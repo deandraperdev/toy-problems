6f4523b06f10e10c45895a7f43b813c59c69fc9d
Dwayne Draper <deandraper82@gmail.com>
2021-01-06 11:50:45 -0600

    Finished balancedParens.js

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -3,18 +3,13 @@
  * the parentheses are balanced and false otherwise.
  *
  * Example:
- *   balancedParens('(');  // false
- *   balancedParens('()'); // true
- *   balancedParens(')(');  // false
- *   balancedParens('(())');  // true
+
  *
  * Step 2:
  *   make your solution work for all types of brackets
  *
  * Example:
- *  balancedParens('[](){}'); // true
- *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
+
  *
  * Step 3:
  * ignore non-bracket characters
@@ -24,6 +19,36 @@
  *
  */
 var balancedParens = function(input) {
+    let split = input.split('');
+    let checkArray = [];
+    for (let i = 0; i < split.length; i++) {
+        if (split[i] === '{' ||split[i] === '[' ||split[i] === '(') {
+            checkArray.push(split[i])
+        }
+        if (split[i] === '}') {
+            if (checkArray.length === 0 || checkArray.pop() !== '{') {
+                return false;
+            }
+        }
+        if (split[i] === ']') {
+            if (checkArray.length === 0 || checkArray.pop() !== '[') {
+                return false;
+            }
+        }
+        if (split[i] === ')') {
+            if (checkArray.length === 0 || checkArray.pop() !== '(') {
+                return false;
+            }
+        }
+    }
+    return true;
 };
+console.log('--------------------------')
+console.log(balancedParens('('));  // true now that we ignore non-bracket chars
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
 
-
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
