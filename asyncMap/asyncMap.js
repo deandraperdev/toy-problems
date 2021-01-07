913788a08196daa8c122e0247411c0a9fe17e236
Dwayne Draper <deandraper82@gmail.com>
2021-01-06 19:37:49 -0600

    Completed asyncMap.js

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -39,4 +39,25 @@
 
 
 var asyncMap = function(tasks, callback) {
+    let promises = tasks.map(task => { return new Promise(task) });
+    return Promise.all(promises)
+        .then(results => {callback(results)})
 };
+
+asyncMap([
+        function(cb){
+            setTimeout(function(){
+                cb('one');
+            }, 200);
+        },
+        function(cb){
+            setTimeout(function(){
+                cb('two');
+            }, 100);
+        }
+    ],
+    function(results){
+        // the results array will equal ['one','two'] even though
+        // the second function had a shorter timeout.
+        console.log(results); // ['one', 'two']
+    });
