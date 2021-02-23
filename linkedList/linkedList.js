46ccdd9baa6135edb59e5b550d3c6d8a609e827b
Dwayne Draper <deandraper82@gmail.com>
2021-02-22 20:15:40 -0600

    Completed linkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,41 +1,44 @@
-/*
- * Implement a linked list using the pseudoclassical instantiation pattern.
- *
- * Your linked list should have methods called "addToTail", "removeHead", and "contains."
- *
- */
-
-// EXAMPLE USAGE:
-// var list = new LinkedList();
-// list.tail;         //yields 'null'
-// list.addToTail(4);
-// list.addToTail(5);
-// list.head.value;   //yields '4';
-// list.contains(5);  //yields 'true';
-// list.contains(6);  //yields 'false';
-// list.removeHead(); //yields '4';
-// list.tail.value;   //yields '5';
-// list.removeHead(); //yields '5';
-// list.removeHead(); //yields 'null';
-
-
 var LinkedList = function() {
   //fill me in!
+
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-) {
+LinkedList.prototype.addToTail = function(value) {
+  let node = this.makeNode(value);
+  if (this.head === null) {
+    this.head = node;
+    this.tail = node;
+  }
+  this.tail.next = node
+  this.tail = node;
 };
 
 LinkedList.prototype.removeHead = function() {
+  let node = this.head;
+  if (this.head !== null) {
+    this.head = this.head.next
+  }
+  return node.value;
 };
 
-LinkedList.prototype.contains = function(
-) {
+LinkedList.prototype.contains = function(value) {
+  let pointer = this.head;
+  while (pointer !== null) {
+    if (pointer.value === value) {
+      return true;
+    }
+    pointer = pointer.next;
+  }
+  return false;
 };
 
-LinkedList.prototype.makeNode = function(
-) {
+LinkedList.prototype.makeNode = function(value) {
+  let node = {};
+  node.value = value;
+  node.next = null;
+  return node;
 };
