d6d64d0c1a57c45091e6411d0707d988a2454c14
Dwayne Draper <deandraper82@gmail.com>
2021-03-21 12:04:34 -0500

    Fixed object reference errors. Problem actually complete now

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -16,32 +16,36 @@ var getIndexBelowMaxForKey = function(str, max) {
   return hash % max;
 };
 
-// I rewrote this a little bit to avoid unnecessary complexity.
-// I changed from a storage array in closure to a storage property on the obhject.
-// This way I can access the different key value pairs in storage directly.
-// It prevents me from having to write getters and setters to retrieve values.
-// In the interest of completing this in an hour, I believe it's the best solution.
-
 const makeHashTable = () => {
   let result = {};
-  result.storage = [];
-  result.storageLimit = 1000;
-  result.insert = () => {
-    let index = getIndexBelowMaxForKey(k, this.storageLimit)
-    let obj = this.storage[index] || {[k]: v};
-    obj[k] = v;
-    this.storage[index] = value;
+  let storage = [];
+  let storageLimit = 1000;
+  result.insert = (k, v) => {
+    console.log('k, v, storage', k, v, storageLimit)
+    let index = getIndexBelowMaxForKey(k, storageLimit)
+    // let obj = storage[index] || {[index]: v};
+    if (storage[index] === undefined) {
+      storage[index] = {[k]: v};
+    } else {
+      let obj = storage[index];
+      obj[k] = v;
+      storage[index] = obj;
+    }
   }
   result.retrieve = (k) => {
-    let index = getIndexBelowMaxForKey(k, this.storageLimit);
-    let obj = this.storage[index];
+    console.log('storage', storage);
+    let index = getIndexBelowMaxForKey(k, storageLimit);
+    console.log('index', index);
+    if (storage[index] === undefined) return 'Value not found';
+    let obj = storage[index];
     return obj[k];
   }
-  result.remove = (keys) => {
-    let index = getIndexBelowMaxForKey(k, this.storageLimit);
-    let obj = this.storage[index];
+  result.remove = (k) => {
+    let index = getIndexBelowMaxForKey(k, storageLimit);
+    let obj = storage[index];
+    console.log('objects', obj, obj[k]);
     delete obj[k];
-    this.storage[index] = value;
+    storage[index] = undefined;
   }
   return result;
 }
