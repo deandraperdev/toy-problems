0acbf7b91cd95a18e13395c2ed024842a8dc2436
Dwayne Draper <deandraper82@gmail.com>
2021-03-21 08:17:08 -0500

    Completed hashTable

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -16,24 +16,32 @@ var getIndexBelowMaxForKey = function(str, max) {
   return hash % max;
 };
 
-var makeHashTable = function() {
-  var result = {};
-  var storage = [];
-  var storageLimit = 1000;
-  result.insert = function(/*...*/ 
-) {
-    // TODO: implement `insert()`
-  };
+// I rewrote this a little bit to avoid unnecessary complexity.
+// I changed from a storage array in closure to a storage property on the obhject.
+// This way I can access the different key value pairs in storage directly.
+// It prevents me from having to write getters and setters to retrieve values.
+// In the interest of completing this in an hour, I believe it's the best solution.
 
-  result.retrieve = function(/*...*/ 
-) {
-    // TODO: implement `retrieve()`
-  };
-
-  result.remove = function(/*...*/ 
-) {
-    // TODO: implement `remove()`
-  };
-
+const HashTable = () => {
+  let result = {};
+  result.storage = [];
+  result.storageLimit = 1000;
+  result.insert = () => {
+    let index = getIndexBelowMaxForKey(k, this.storageLimit)
+    let obj = this.storage[index] || {[k]: v};
+    obj[k] = v;
+    this.storage[index] = value;
+  }
+  result.retrieve = (k) => {
+    let index = getIndexBelowMaxForKey(k, this.storageLimit);
+    let obj = this.storage[index];
+    return obj[k];
+  }
+  result.remove = (keys) => {
+    let index = getIndexBelowMaxForKey(k, this.storageLimit);
+    let obj = this.storage[index];
+    delete obj[k];
+    this.storage[index] = value;
+  }
   return result;
-};
+}
