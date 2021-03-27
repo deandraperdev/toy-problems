df4f57067c5d8e0928589992e545b598a378853d
Dwayne Draper <deandraper82@gmail.com>
2021-03-27 11:22:44 -0500

    eslint

--- numberToEnglish/numberToEnglish.js
+++ numberToEnglish/numberToEnglish.js
@@ -53,6 +53,55 @@ var numbersToPlace = {
   1000000000000000000: 'quintillion',
 };
 
-Number.prototype.toEnglish = function () {
+Number.prototype.toEnglish = function (number) {
   // return my value as english words
+  let result = '';
+  let array = number.toString().split('');
+  let tens = parseInt('1' + '0'.repeat(array.length - 1));
+  // if a number is 20 or less, use i++ so loop will skip a number
+  for (let i = 0; i < array.length; i++) {
+    let str = '';
+    if (numbersToPlace.tens !== undefined) {
+      if (tens.length % 3 === 2)
+      str += numbersToWords[i];
+      str += numbersToPlace[i];
+    } else
+  }
 };
+
+/*
+Input = number
+Output = string that is the spoken form of the number
+
+Calling this function turn a raw number into the spoken/written form of said number;
+
+We can split the numbers into an array, then use each element of the array to get the spoken form;
+We will count the length of the array to a string of the actual number form: '1000000000';
+Each iteration we will remove a zero with chatAt(length - 1);
+For each tens place, we will create a string for that tens place, and then concat them all;
+For numbers under 100 at the tens place, we will break this rule, and just use the written form;
+For numbers also under 20 at the tens place, we will use a similar method to the overall
+
+6543 --> [6, 5, 4, 3];
+array.length = 4 --> - 1 = 3 --> number of zeros --> 1,000
+At each multiple of 3 zeros, add thousand, million, billion, etc
+n % 3 === 0
+
+[6, 5, 4, 3] && 1,000 (3 zeros)
+ ^
+str = 'six thousand '
+[6, 5, 4, 3] && 100
+    ^
+str = str + 'five hundred '
+str = 'six thousand five hundred '
+[6, 5, 4, 3] && 10
+       ^
+over 20 and under 100
+str = str + 'forty '
+str = 'six thousand five hundred forty '
+[6, 5, 4, 3]
+          ^
+str = str + 'three'
+str = 'six thousand five hundred forty three'
+return str;
+*/
